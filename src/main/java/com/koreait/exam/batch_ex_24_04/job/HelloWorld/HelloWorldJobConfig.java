a947ab1dad21a751d89070c6b3e5cfa379b4b70f
JUNGmoon123 <jungpro567@gmail.com>
2024-04-17 14:10:27 +0900

    Bean, Job, Step Scope 설명 및 추가

--- src/main/java/com/koreait/exam/batch_ex_24_04/job/HelloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/job/HelloWorld/HelloWorldJobConfig.java
@@ -1,41 +1,50 @@
 package com.koreait.exam.batch_ex_24_04.job.HelloWorld;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class HelloWorldConfig {
+public class HelloWorldJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job helloWorldJob(Step helloWorldStep1) {
-        return jobBuilderFactory.get("helloWorldJob").start(helloWorldStep1).build();  //jobbuilder를 가져오는것
-        
-    }
-    @Bean
-    public Step helloWorldStep1(Tasklet helloWorldTasklet) {
-        return stepBuilderFactory.get("helloWorldStep1").tasklet(helloWorldTasklet).build();
+    public Job helloWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID 를 실행할 때 파라미터로 부여
+                .start(helloWorldStep1())
+                .build();
     }
 
     @Bean
+    @JobScope
+    public Step helloWorldStep1() {
+        return stepBuilderFactory
+                .get("helloWorldStep1")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @Bean
+    @StepScope
     public Tasklet helloWorldTasklet() {
         return (stepContribution, chunkContext) -> {
-            System.out.println("헬로월드헬로월드!!!");
+
+            System.out.println("헬로월드!!!");
+
             return RepeatStatus.FINISHED;
         };
     }
-    // Job : 여러가지의 Step들로 구성
+//     Job : 여러가지의 Step들로 구성
 }
